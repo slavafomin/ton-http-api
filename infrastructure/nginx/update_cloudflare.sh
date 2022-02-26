00d56ec161f61a45e95cdf3798e694907b564088
dungeon-master-666 <dungeon666master@protonmail.com>
2022-02-26 19:21:26 +0100

    remove ip restriction

--- infrastructure/nginx/update_cloudflare.sh
+++ infrastructure/nginx/update_cloudflare.sh
@@ -6,21 +6,6 @@ CLOUDFLARE_FILE_PATH=/etc/nginx/cloudflare
 
 echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
 
-
-echo "geo \$realip_remote_addr \$is_cf_allowed {" >> $CLOUDFLARE_FILE_PATH;
-echo "  default 0;" >> $CLOUDFLARE_FILE_PATH;
-for i in ${TON_API_WHITELISTED_IPS//:/ }
-do
-    echo "  $i 1;" >> $CLOUDFLARE_FILE_PATH;
-done
-for i in `curl -sL https://www.cloudflare.com/ips-v4`; do
-    echo "  $i 1;" >> $CLOUDFLARE_FILE_PATH;
-done
-for i in `curl -sL https://www.cloudflare.com/ips-v6`; do
-    echo "  $i 1;" >> $CLOUDFLARE_FILE_PATH;
-done
-echo "}" >> $CLOUDFLARE_FILE_PATH;
-
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
 for i in `curl -sL https://www.cloudflare.com/ips-v4`; do
